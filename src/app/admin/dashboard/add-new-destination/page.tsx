7b939a90d943d6d6e709a30043ab5e8d39097fa6
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-07-05 16:38:23 +0530

    can now add space in description of destination

--- src/app/admin/dashboard/add-new-destination/page.tsx
+++ src/app/admin/dashboard/add-new-destination/page.tsx
@@ -193,7 +193,7 @@ export default function AddNewDestinationPage() {
                                             name="destination-description"
                                             rows={5}
                                             value={destinationDescription}
-                                            onChange={(e) => setDestinationDescription(e.target.value.trim())}
+                                            onChange={(e) => setDestinationDescription(e.target.value)}
                                             required
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
                                             placeholder="A Brief Description"
