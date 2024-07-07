3c4532ba261c5f42ee5874944436d25390642c65
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-07-07 11:47:09 +0530

    Added pricing in seperate components to prevent ungly loading screen

--- src/app/destination/[destinationId]/package/[packageId]/page.tsx
+++ src/app/destination/[destinationId]/package/[packageId]/page.tsx
@@ -16,6 +16,7 @@ import {useRouter} from "next/navigation";
 
 import {Package, DestinationData} from "@/app/_utility/types";
 import LoadingSpinner from "@/app/components/LoadingSpinner";
+import PricingComponent from "@/app/destination/[destinationId]/package/[packageId]/PricingComponents";
 
 
 const Err404 = dynamic(() => import("@/app/404/page"), {ssr:false})
@@ -129,28 +130,7 @@ export default function Page({params}: { params: { destinationId: string, packag
 
 
                                 <div className="hidden lg:block lg:col-span-4">
-                                    <div>
-                                        <div className="border-b border-gray-200 pb-5">
-                                            <h3 className="leading-6 font-bold text-2xl text-gray-900">Details</h3>
-                                        </div>
-                                        <p>
-                                            <h3 className="text-lg text-gray-800 pt-5 flex items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                     viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-                                                     className="size-6 mr-3">
-                                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                                          d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                                                </svg>
-
-                                                {packageData.duration}</h3>
-                                            <span
-                                                className="text-[#008DDA] text-2xl font-bold pr-1 mt-3 ">₹ {packageData.discountedPrice}</span>
-                                            <span
-                                                className="text-gray-500 font-medium text-lg line-through mt-3">₹ {packageData.originalPrice} </span>
-                                            <span
-                                                className="ml-1 text-neutral-700 font-sans text-sm mt-3 ">Per person</span>
-                                        </p>
-                                    </div>
+                                    <PricingComponent packageData={packageData} />
                                     <nav aria-label="Sidebar" className="sticky top-6 divide-y divide-gray-300">
                                         <ContactFormSidebar sidebar={true} heading={'Book Now!'} destinationId={destinationData.id}
                                                             packageId={packageData.id}/>
