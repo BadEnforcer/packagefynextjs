a67456464c460b16fd9c56c05421ac29a9f35a64
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-08-10 01:35:13 +0530

    Footer has team link now

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -17,6 +17,7 @@ const footerNavigation = {
     ],
     company: [
         {name: 'About us', href: '/about'},
+        {name: 'Team', href: '/about/#team-info'},
         {name: 'Jobs', href: 'mailto:Packagefytour@gmail.com?subject=I%20am%20interested%20in%20working%20for%20your%20organization'},
         {name: 'Partners', href: '/#recognition-and-collaboration'},
     ],
