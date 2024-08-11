aa1b9917282a162c7a06c9ae6b2d550245237143
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-08-11 21:59:35 +0530

    Updated FeviIcon and Used next/Head of Head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,28 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata: Metadata = {
     title: "Packagefy",
     description: "Packagefy - Find your best holiday package. Follow us on Facebook, LinkedIn, and Instagram for the latest updates.",
+    icons: {
+        icon: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.svg?alt=media&token=833503db-fc0a-4385-9779-58abdd059d78',
+    },
+    applicationName : 'Packagefy.com Website',
+    openGraph : {
+        title: 'Packagefy',
+        description: "Packagefy - Find your best holiday package.",
+        images: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.svg?alt=media&token=833503db-fc0a-4385-9779-58abdd059d78',
+        type: 'website',
+        siteName: 'Packagefy'
+    },
+    twitter : {
+        title: 'Packagefy',
+        card: 'summary_large_image',
+        description: "Packagefy - Find your best holiday package.",
+        images: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.svg?alt=media&token=833503db-fc0a-4385-9779-58abdd059d78',
+        creator: 'packagefy',
+        creatorId: 'packagefy',
+    },
+    authors : [{name: 'Raj Dwivedi', url: 'https://rajdwivedi.space'}],
+    robots: {follow: true, index: true},
+
 };
 
 export default function RootLayout({
@@ -31,44 +53,13 @@ export default function RootLayout({
 
         <html lang="en">
         <Head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-            {/* Meta Description */}
-            <meta name="description" content="Packagefy - Find your best holiday package. Follow us on Facebook, LinkedIn, and Instagram for the latest updates." />
-
-            {/* Open Graph Meta Tags */}
-            <meta property="og:title" content="Packagefy" />
-            <meta property="og:description" content="Packagefy - Find your best holiday package." />
-            <meta property="og:image" content="/src/assets/logo.png" />
-            <meta property="og:url" content="https://packagefy.com" />
-            <meta property="og:type" content="website" />
-            <meta property="og:site_name" content="Packagefy" />
-
-            {/* Twitter Meta Tags */}
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="Packagefy" />
-            <meta name="twitter:description" content="Packagefy - Find your best holiday package." />
-            <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.svg?alt=media&token=833503db-fc0a-4385-9779-58abdd059d78" />
-            <meta name="twitter:url" content="https://x.com/packagefy" />
-
-            {/* Favicon */}
-            <link rel="icon" type="image/png" href="https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.svg?alt=media&token=833503db-fc0a-4385-9779-58abdd059d78" />
-
             {/* Canonical Link */}
             <link rel="canonical" href="https://packagefy.com" />
-
-            {/* RSS Feed */}
-            {/*<link rel="alternate" type="application/rss+xml" title="RSS Feed for Packagefy" href="https://packagefy.com/rss.xml" />*/}
 
             {/* Social Media Profiles */}
             <link rel="me" href="https://www.facebook.com/Packagefy" />
             <link rel="me" href="https://www.linkedin.com/company/packagefy/" />
             <link rel="me" href="https://www.instagram.com/packagefy/" />
-
-            {/* Additional SEO Tags */}
-            <meta name="robots" content="index, follow" />
-            <meta name="theme-color" content="#4f46e5" />
 
             {/* Structured Data */}
             <script type="application/ld+json">
@@ -88,8 +79,11 @@ export default function RootLayout({
                     `}
             </script>
 
-            <Script src="https://www.googletagmanager.com/gtag/js?l=dataLayer&amp;id=G-4L0BZGV7V7" async></Script>
         </Head>
+
+
+        <Script src="https://www.googletagmanager.com/gtag/js?l=dataLayer&amp;id=G-4L0BZGV7V7" async></Script>
+
         <SpeedInsights/>
         <Analytics />
 
