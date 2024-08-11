eece3f30cd78fde9965f181262b286fcfda8db47
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-08-11 19:03:08 +0530

    Update sitemap page

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             {
                 userAgent: '*',
                 allow: ['/destination/*', '/destination/*/package/*', '/'],
-                disallow: '/api/',
+                disallow: ['/api/', '/admin/'],
             }
         ],
         sitemap: 'https://packagefy.com/sitemap.xml',
