78da34d4559bcbdd02261957e53c2ef258d98b0d
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-08-11 18:52:59 +0530

    Update sitemap page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: 'https://packagefy.com', // Replace with your site's URL
-    generateRobotsTxt: false, // (optional) Generate a robots.txt file
-    // sitemapSize: 5000, // (optional) Limits the number of URLs in a single sitemap file
-    outDir: './public', // (optional) The output directory for the generated sitemap files
-}
+    siteUrl: 'https://packagefy.com',
+    generateRobotsTxt: false,
+    sitemapSize: 5000, // Adjust this to include all URLs in one file if desired
+    outDir: './public',
+};
 
 module.exports = config;
+
