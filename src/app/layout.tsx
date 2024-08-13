eb6f9dad2c432bcda4bd012a20735aeecfd111c2
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-08-13 18:18:13 +0530

    update title and description for og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,24 +17,24 @@ const inter = Inter({subsets: ["latin"]});
 
 
 export const metadata: Metadata = {
-    title: "Packagefy",
-    description: "Packagefy - Find your best holiday package. Follow us on Facebook, LinkedIn, and Instagram for the latest updates.",
+    title: "Packagefy - Find your best holiday package.",
+    description: "Solution for all your travel needs. We make travel better and easier for people of India. Follow us on Facebook, LinkedIn, and Instagram for the latest updates.",
     icons: {
         icon: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.svg?alt=media&token=833503db-fc0a-4385-9779-58abdd059d78',
     },
 
     applicationName : 'Packagefy.com Website',
     openGraph : {
-        title: 'Packagefy',
-        description: "Packagefy - Find your best holiday package.",
+        title: 'Packagefy - Find your best holiday package.',
+        description: "Solution for all your travel needs. We make travel better and easier for people of India.",
         images: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.png?alt=media&token=923e7d4c-912d-48ed-99fa-946a2247f49c',
         type: 'website',
         siteName: 'Packagefy'
     },
     twitter : {
-        title: 'Packagefy',
+        title: 'Packagefy - Find your best holiday package.',
         card: 'summary_large_image',
-        description: "Packagefy - Find your best holiday package.",
+        description: "Solution for all your travel needs. We make travel better and easier for people of India.",
         images: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.png?alt=media&token=923e7d4c-912d-48ed-99fa-946a2247f49c',
         creator: 'packagefy',
         creatorId: 'packagefy',
