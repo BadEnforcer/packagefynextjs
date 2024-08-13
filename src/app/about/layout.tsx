029f61aa7a3f932292e023ecd008e63f0cfaf950
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-08-13 17:51:36 +0530

    Updated about us, changed logo url for open graph and twitter.
    the feviicon uses the svg.

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,39 +1,43 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "./globals.css";
+import ".././globals.css";
 import React from "react";
 import {SpeedInsights} from "@vercel/speed-insights/next"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./customSlickTheme.css"
 import dynamic from "next/dynamic";
-import 'react-toastify/dist/ReactToastify.css';
-import {ToastContainer} from "react-toastify";
 import { Analytics } from "@vercel/analytics/react"
-const PrelineScript = dynamic(() => import('./components/PrelineScript'))
+const PrelineScript = dynamic(() => import('../components/PrelineScript'))
 import Script from "next/script";
 
 const inter = Inter({subsets: ["latin"]});
 
 
 export const metadata: Metadata = {
-    title: "Packagefy",
-    description: "Packagefy - Find your best holiday package. Follow us on Facebook, LinkedIn, and Instagram for the latest updates.",
+    title: "About us",
+    description:
+        "solution for all your travel needs. We make travel better and easier for people of India. Packagefy ideally caters " +
+        "to a diverse range of customers to maximize its market reach and appeal." +
+        "This includes:  Leisure Travelers, Business Travelers, Adventure Seekers, Budget-conscious Travelers Offering affordable options, Honeymooners, Educational Student tours.",
     icons: {
         icon: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.svg?alt=media&token=833503db-fc0a-4385-9779-58abdd059d78',
     },
     applicationName : 'Packagefy.com Website',
     openGraph : {
-        title: 'Packagefy',
-        description: "Packagefy - Find your best holiday package.",
+        title: 'About us',
+        description:
+            "solution for all your travel needs. We make travel better and easier for people of India. Packagefy ideally caters " +
+            "to a diverse range of customers to maximize its market reach and appeal." +
+            "This includes:  Leisure Travelers, Business Travelers, Adventure Seekers, Budget-conscious Travelers Offering affordable options, Honeymooners, Educational Student tours.",
         images: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.png?alt=media&token=923e7d4c-912d-48ed-99fa-946a2247f49c',
         type: 'website',
         siteName: 'Packagefy'
     },
     twitter : {
-        title: 'Packagefy',
+        title: 'About us - Packagefy',
         card: 'summary_large_image',
-        description: "Packagefy - Find your best holiday package.",
+        description:
+            "solution for all your travel needs. We make travel better and easier for people of India. Packagefy ideally caters " +
+            "to a diverse range of customers to maximize its market reach and appeal." +
+            "This includes:  Leisure Travelers, Business Travelers, Adventure Seekers, Budget-conscious Travelers Offering affordable options, Honeymooners, Educational Student tours.",
         images: 'https://firebasestorage.googleapis.com/v0/b/packagefy.appspot.com/o/resources%2Fnewlogo.png?alt=media&token=923e7d4c-912d-48ed-99fa-946a2247f49c',
         creator: 'packagefy',
         creatorId: 'packagefy',
@@ -53,13 +57,10 @@ export default function RootLayout({
         <html lang="en">
 
         <Script src="https://www.googletagmanager.com/gtag/js?l=dataLayer&amp;id=G-4L0BZGV7V7" async></Script>
-
         <SpeedInsights/>
         <Analytics />
 
         <body className={inter.className}>
-        <ToastContainer/>
-
         {children}
         <PrelineScript/>
 
