4867d911bcc0513647d4abcebd38fa8ae6552764
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-09-11 11:16:03 +0530

    remove 12 ls hotfix v2 update herosearch toast v2

--- src/app/components/HeroSearch.tsx
+++ src/app/components/HeroSearch.tsx
@@ -42,8 +42,8 @@ export default function HeroSearch() {
                 setIsLoading(false);
             } catch (err) {
                 setIsLoading(false);
-                // toast.error("Failed to fetch search items. Server Error");
-                toast.info("Failed to fetch search data from server.");
+                toast.error("Search has crashed");
+                toast.info("Request rejected by server.");
             }
         };
         fetchDestinations().then(() => {});
